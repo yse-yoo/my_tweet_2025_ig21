ef5d0c082883842563564831eae317086c85e158
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-22 10:55:25 +0900

    update

--- components/nav.php
+++ components/nav.php
@@ -1,7 +1,7 @@
 <nav id="side-menu" class="p-3">
     <ul>
         <li>
-            <a href="" class="inline-flex items-center">
+            <a href="home/" class="inline-flex items-center">
                 <img src="svg/home.svg" class="w-10 mr-2">
                 <span class="hidden md:inline">ホーム</span>
             </a>
@@ -12,6 +12,9 @@
             </div>
             <!-- ポップアップ（初期状態は非表示） -->
             <div id="user-popup" class="hidden absolute left-0 m-2 w-48 bg-white border border-gray-300 rounded shadow-lg z-10">
+                <a href="user/" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                    ユーザホーム
+                </a>
                 <a href="user/logout.php" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                     <?= $auth_user['account_name'] ?>
                     からログアウト
