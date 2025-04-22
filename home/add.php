77987eb80ff557163e31f2de0328da2497e47c83
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-22 09:44:33 +0900

    update

--- home/add.php
+++ home/add.php
@@ -2,12 +2,18 @@
 // 共通ファイル app.php を読み込み
 require_once('../app.php');
 
+use App\Models\AuthUser;
+
 // POSTリクエスト以外は処理しない
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 
 // TODO: ログインユーザチェック
+$auth_user = AuthUser::checkLogin();
+// var_dump($auth_user);
+// exit;
+
 // TODO: ユーザがいなかったらログイン画面にリダイレクト
 // TODO: POSTデータを取得
 // TODO: 投稿処理
